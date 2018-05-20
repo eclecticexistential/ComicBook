1a43d4f66e623ce21d49810001805a817c81f577
Earthling <Earthling@EARTHLING-PC>
2018-05-20 15:55:13 -0700

    Reviewed steps to create ASP.NET application. Left out HTML.raw markup.

--- ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/Controllers/ComicBooksController.cs
@@ -1,4 +1,5 @@
-﻿using ComicBookGallery.Models;
+﻿using ComicBookGallery.Data;
+using ComicBookGallery.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,22 +10,27 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
-        public ActionResult Detail()
+        private ComicBookRepository _comicBookRepository = null;
+
+        public ComicBooksController()
         {
-            var comicBook = new ComicBook()
+            _comicBookRepository = new ComicBookRepository();
+        }
+
+        public ActionResult Index()
+        {
+            var comicBooks = _comicBookRepository.GetComicBooks();
+            return View(comicBooks);
+        }
+
+        public ActionResult Detail(int? id)
+        {
+            if(id == null)
             {
-                SeriesTitle = "",
-                IssueNumber = 700,
-                Description = "",
-                Artists = new Artist[]
-                {
-                    new Artist() {Name= "", Role = ""},
-                    new Artist() {Name= "", Role = ""},
-                    new Artist() {Name= "", Role = ""},
-                    new Artist() {Name= "", Role = ""}
-                }
-            };
-            return View();
+                return HttpNotFound();
+            }
+            var comicBook = _comicBookRepository.GetComicBook((int)id);
+            return View(comicBook);
         }
     }
 }
---
+++ ComicBookGallery/Data/ComicBookRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ComicBookGallery.Models;
+
+namespace ComicBookGallery.Data
+{
+    public class ComicBookRepository
+    {
+        private static ComicBook[] _comicBooks = new ComicBook[]
+        {
+            new ComicBook()
+            {
+                Id = 0,
+                SeriesTitle = "",
+                IssueNumber = 0,
+                Description = "",
+                Artists = new Artist[]
+                {
+                    new Artist() {Name= "", Role = ""},
+                    new Artist() {Name= "", Role = ""},
+                    new Artist() {Name= "", Role = ""},
+                    new Artist() {Name= "", Role = ""}
+                }
+            }
+        };
+        public ComicBook[] GetComicBooks()
+        {
+            return _comicBooks;
+        }
+        public ComicBook GetComicBook(int id)
+        {
+            ComicBook comicBookToReturn = null;
+            foreach (var comicBook in _comicBooks)
+            {
+                if (comicBook.Id == id)
+                {
+                    comicBookToReturn = comicBook;
+                    break;
+                }
+            }
+            return comicBookToReturn;
+        }
+    }
+}
