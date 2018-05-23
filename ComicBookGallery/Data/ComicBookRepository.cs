c1d00e29d0f315a43659822fe1632b953a1982bc
Earthling <Earthling@EARTHLING-PC>
2018-05-22 22:52:46 -0700

    Added images and comic data. Manually fixed errors.

--- ComicBookGallery/Data/ComicBookRepository.cs
+++ ComicBookGallery/Data/ComicBookRepository.cs
@@ -13,16 +13,83 @@ namespace ComicBookGallery.Data
             new ComicBook()
             {
                 Id = 0,
-                SeriesTitle = "",
-                IssueNumber = 0,
-                Description = "",
+                SeriesTitle = "Black Panther",
+                IssueNumber = 1,
+                Description = "Flags are burning.",
+                Artists = new Artist[]
+                {
+                    new Artist() {Name= "Ta-Nehisi Coates", Role = ""},
+                    new Artist() {Name= "Brian Stelfreeze", Role = ""},
+                    new Artist() {Name= "Laura Martin", Role = ""}
+                },
+                Favorite = true
+            },
+            new ComicBook()
+            {
+                Id = 1,
+                SeriesTitle = "Captain America",
+                IssueNumber = 695,
+                Description = "Fight more!",
+                Artists = new Artist[]
+                {
+                    new Artist() {Name= "Mark Waid", Role = ""},
+                    new Artist() {Name= "Chris Samnee", Role = ""},
+                    new Artist() {Name= "Matthew Wilson", Role = ""}
+                }
+            },
+            new ComicBook()
+            {
+                Id = 2,
+                SeriesTitle = "Captain Marvel",
+                IssueNumber = 4,
+                Description = "Battle across the skies.",
+                Artists = new Artist[]
+                {
+                    new Artist() {Name= "Fazekas", Role = ""},
+                    new Artist() {Name= "Butters", Role = ""},
+                    new Artist() {Name= "Anka", Role = ""},
+                    new Artist() {Name= "Wilson", Role = ""}
+                }
+            },
+            new ComicBook()
+            {
+                Id = 3,
+                SeriesTitle = "Moon Knight",
+                IssueNumber = 1,
+                Description = "Battle across the skies.",
+                Artists = new Artist[]
+                {
+                    new Artist() {Name= "Lemire", Role = ""},
+                    new Artist() {Name= "Smallwood", Role = ""},
+                    new Artist() {Name= "Bellaire", Role = ""}
+                }
+            },
+            new ComicBook()
+            {
+                Id = 4,
+                SeriesTitle = "The Amazing Spiderman",
+                IssueNumber = 316,
+                Description = "The non-mutant superhero!",
                 Artists = new Artist[]
                 {
                     new Artist() {Name= "", Role = ""},
                     new Artist() {Name= "", Role = ""},
-                    new Artist() {Name= "", Role = ""},
                     new Artist() {Name= "", Role = ""}
                 }
+            },
+            new ComicBook()
+            {
+                Id = 5,
+                SeriesTitle = "Rick & Morty",
+                IssueNumber = 30,
+                Description = "The non-mutant superhero!",
+                Artists = new Artist[]
+                {
+                    new Artist() {Name= "Starks", Role = ""},
+                    new Artist() {Name= "Dewey", Role = ""},
+                    new Artist() {Name= "Ellerby", Role = ""}
+                },
+                Favorite = true
             }
         };
         public ComicBook[] GetComicBooks()
